9a647f64512f2bad663237a8e75900aac0237307
pjsph <pierrepangames@gmail.com>
2023-06-08 17:09:31 +0200

    Image editor

--- src/Pages/Edit/Edit.jsx
+++ src/Pages/Edit/Edit.jsx
@@ -1,20 +1,44 @@
 import React from "react";
 import './edit.css';
+import 'tui-image-editor/dist/tui-image-editor.css';
+import ImageEditor from '@toast-ui/react-image-editor';
 
 const Edit = () => {
     return (
-        <header>
-            <div className="container edit__container">
-                <h5>Edit</h5>
-                <h1>your meme</h1>
-                <h5 className="text-light">Add custom quotes, etc.</h5>
-                
-                <div className="cta">
-                <a href="" download className="btn btn-primary">Download</a>
-                <a href="" className="btn">Reset</a>
+        <>
+            <header>
+                <div className="container edit__container">
+                    <h5>Edit</h5>
+                    <h1>your meme</h1>
+                    <h5 className="text-light">Add custom quotes, etc.</h5>
+                </div>
+            </header>
+            <div className="editor">
+                <ImageEditor
+                    includeUI={{
+                        loadImage: {
+                            path: './logo512.png',
+                            name: 'SampleImage',
+                        },
+                        theme: {},
+                        menu: ['shape', 'filter', 'text'],
+                        initMenu: 'filter',
+                        uiSize: {
+                            width: '90%',
+                            height: '70%',
+                        },
+                        menuBarPosition: 'bottom',
+                    }}
+                    cssMaxHeight={500}
+                    cssMaxWidth={700}
+                    selectionStyle={{
+                        cornerSize: 20,
+                        rotatingPointOffset: 70,
+                    }}
+                    usageStatistics={true}
+                />
             </div>
-            </div>
-        </header>
+        </>
     )
 }
 
