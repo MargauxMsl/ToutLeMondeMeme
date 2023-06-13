f2fd438f98adc99e4894b11944076a2e8cee0326
pjsph <pierrepangames@gmail.com>
2023-06-13 17:18:26 +0200

    Add go back button on edit page

--- src/Pages/Edit/Edit.jsx
+++ src/Pages/Edit/Edit.jsx
@@ -2,104 +2,111 @@ import React, { useEffect, useState, useRef } from "react";
 import './edit.css';
 import 'tui-image-editor/dist/tui-image-editor.css';
 import ImageEditor from '@toast-ui/react-image-editor';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 
 const Edit = () => {
-  const { id } = useParams();
-  const imageEditorRef = useRef(null);
-  const fileInputRef = useRef(null);
-  const [meme, setMeme] = useState(null);
+    const { id } = useParams();
+    const imageEditorRef = useRef(null);
+    const fileInputRef = useRef(null);
+    const [meme, setMeme] = useState(null);
+    const navigate = useNavigate();
 
-  useEffect(() => {
-    fetch('http://localhost:3001/api/memes')
-      .then(response => response.json())
-      .then(data => {
-        const foundMeme = data.find(meme => meme.id === id);
-        setMeme(foundMeme);
-      })
-      .catch(error => {
-        console.error('Error fetching meme data:', error);
-      });
-  }, [id]);
+    useEffect(() => {
+        fetch('http://localhost:3001/api/memes')
+            .then(response => response.json())
+            .then(data => {
+                const foundMeme = data.find(meme => meme.id === id);
+                setMeme(foundMeme);
+            })
+            .catch(error => {
+                console.error('Error fetching meme data:', error);
+            });
+    }, [id]);
 
-  if (!meme) {
-    return <div>Loading...</div>;
-  }
-
-  const handleDownloadClick = () => {
-    const imageEditorInstance = imageEditorRef.current?.getInstance();
-    if (imageEditorInstance) {
-      const canvasData = imageEditorInstance.toDataURL({
-        format: 'jpeg',
-        quality: 0.8,
-      });
-
-      const downloadLink = document.createElement('a');
-      downloadLink.href = canvasData;
-      downloadLink.download = `edited_meme_${meme.id}.jpeg`;
-      downloadLink.target = '_blank';
-
-      downloadLink.click();
+    if (!meme) {
+        return <div>Loading...</div>;
     }
-  };
 
-  const Loadimagefromdir = (event) => {
-    const file = event.target.files[0];
-    const reader = new FileReader();
+    const handleDownloadClick = () => {
+        const imageEditorInstance = imageEditorRef.current?.getInstance();
+        if (imageEditorInstance) {
+            const canvasData = imageEditorInstance.toDataURL({
+                format: 'jpeg',
+                quality: 0.8,
+            });
 
-    reader.onload = (e) => {
-      const imageEditorInstance = imageEditorRef.current?.getInstance();
-      if (imageEditorInstance) {
-        imageEditorInstance.loadImageFromURL(e.target.result, 'SampleImage');
-      }
+            const downloadLink = document.createElement('a');
+            downloadLink.href = canvasData;
+            downloadLink.download = `edited_meme_${meme.id}.jpeg`;
+            downloadLink.target = '_blank';
+
+            downloadLink.click();
+        }
     };
 
-    reader.readAsDataURL(file);
-  };
+    const Loadimagefromdir = (event) => {
+        const file = event.target.files[0];
+        const reader = new FileReader();
 
-  return (
-    <>
-      <header>
-        <div className="container edit__container">
-          <h5>Edit</h5>
-          <h1>your meme</h1>
-          <h5 className="text-light">Add custom quotes, etc.</h5>
-          
-        </div>
-      </header>
-      <div className="editor">
-        <ImageEditor
-          ref={imageEditorRef}
-          includeUI={{
-            loadImage: {
-              path: meme.url,
-              name: 'SampleImage',
-            },
-            theme: {},
-            menu: ['shape', 'filter', 'text'],
-            initMenu: 'filter',
-            uiSize: {
-              width: '90%',
-              height: '70%',
-            },
-            menuBarPosition: 'bottom',
-          }}
-          cssMaxHeight={500}
-          cssMaxWidth={700}
-          selectionStyle={{
-            cornerSize: 20,
-            rotatingPointOffset: 70,
-          }}
-          usageStatistics={false}
-        />
-        <div className="button">
-            <input type="file" accept="image/*" onChange={Loadimagefromdir} ref={fileInputRef} style={{ display: "none" }} />
-            <button onClick={() => fileInputRef.current.click()}>Load Image</button>
-            <button onClick={handleDownloadClick}>Download</button>
-          </div>
-      </div>
-    </>
-  );
+        reader.onload = (e) => {
+            const imageEditorInstance = imageEditorRef.current?.getInstance();
+            if (imageEditorInstance) {
+                imageEditorInstance.loadImageFromURL(e.target.result, 'SampleImage');
+            }
+        };
+
+        reader.readAsDataURL(file);
+    };
+
+    const handleBackClick = memeId => {
+        navigate('/');
+        //window.open(`/edit/${memeId}`, "_blank");    
+    };
+
+    return (
+        <>
+            <header>
+                <div className="container edit__container">
+                    <h5>Edit</h5>
+                    <h1>your meme</h1>
+                    <h5 className="text-light">Add custom quotes, etc.</h5>
+                </div>
+            </header>
+            <div className="back">
+                <button onClick={handleBackClick}>Go back</button>
+            </div>
+            <div className="editor">
+                <ImageEditor
+                    includeUI={{
+                        loadImage: {
+                            path: meme.url,
+                            name: 'SampleImage',
+                        },
+                        theme: {},
+                        menu: ['shape', 'filter', 'text'],
+                        initMenu: 'filter',
+                        uiSize: {
+                            width: '90%',
+                            height: '70%',
+                        },
+                        menuBarPosition: 'bottom',
+                    }}
+                    cssMaxHeight={500}
+                    cssMaxWidth={700}
+                    selectionStyle={{
+                        cornerSize: 20,
+                        rotatingPointOffset: 70,
+                    }}
+                    usageStatistics={false}
+                />
+                <div className="button">
+                    <input type="file" accept="image/*" onChange={Loadimagefromdir} ref={fileInputRef} style={{ display: "none" }} />
+                    <button onClick={() => fileInputRef.current.click()}>Load Image</button>
+                    <button onClick={handleDownloadClick}>Download</button>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default Edit;
