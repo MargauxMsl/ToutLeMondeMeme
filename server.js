82cf4dabe5e249079b6730752e6e3c87ee4ac626
JakoFK <122287028+JakoFK@users.noreply.github.com>
2023-06-16 08:53:00 +0200

    Connexion au serveur + stockage dans base de données

--- server.js
+++ server.js
@@ -23,10 +23,18 @@ mongoose.connect("mongodb://127.0.0.1:27017/Memes", {
   console.log('Erreur lors de la connexion à MongoDB :', error);
 });
 
+// Assuming you have a Meme model defined with other fields
+const memeSchema = new mongoose.Schema({
+  image: { name: String, type: String, required: true },
+});
+
+
 
 // Enable CORS for all routes
 
 app.use(cors());
+
+app.use(fileUpload()); // Add the file upload middleware
 
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'build')));
@@ -60,6 +68,32 @@ app.get('/memes/:id', (req, res) => {
     res.status(500).json({ error: 'Internal Server Error' });
   }
 });
+
+app.post('/api/memes/store', (req, res) => {
+  const { id, image } = req.body;
+
+  if (!id || !image) {
+    return res.status(400).json({ error: 'Invalid request body' });
+  }
+
+  // Save the file or base64 image directly to the MongoDB document
+  const meme = new Meme({
+    id,
+    image, // Assuming the image field in the Meme schema matches the base64 string or URL format
+  });
+
+  meme
+    .save()
+    .then(() => {
+      console.log('Meme stored in MongoDB');
+      res.sendStatus(200);
+    })
+    .catch((error) => {
+      console.error('Error storing meme in MongoDB:', error);
+      res.sendStatus(500);
+    });
+});
+
 
 // Use json-server middleware
 app.use(jsonServer.defaults());
