07e76423258182f4f9a60150d794dd0203410cec
LenaPolitain <122284840+LenaPolitain@users.noreply.github.com>
2023-06-18 22:56:50 +0200

    Update Meme.jsx

--- src/Pages/Meme/Meme.jsx
+++ src/Pages/Meme/Meme.jsx
@@ -38,7 +38,7 @@ function MemeComponent() {
 
   useEffect(() => {
     axios
-      .get("https://gr66gp-3001.csb.app/api/memes")
+      .get("https://localhost:3001/api/memes")
       .then((response) => {
         const fetchedMemes = response.data;
         const formattedMemes = fetchedMemes.map((meme) => ({
